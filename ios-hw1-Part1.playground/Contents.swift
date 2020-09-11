20f44c2d57ace06bd2be7e5ecce0586a819d5bea
Buasad <hamoodat@live.com>
2020-09-11 21:50:53 +0300

    done

--- ios-hw1-Part1.playground/Contents.swift
+++ ios-hw1-Part1.playground/Contents.swift
@@ -7,10 +7,10 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let name2 = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,17 +38,27 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "mohammed"
+var lastName1 = "alasad"
+var email1 = "hamoodat@live.com"
+var phoneNumber1 = 94490330
+var age1 = 17
+var country1 = "Kuwait"
+var area1 = "West" + " " + "Mishref"
+var password = "Confidential"
+var isKuwaiti = true
 
 //Person 2
+let firstname2 = "ali"
+let lastname2 = "sumait"
+let email2 = "nonexistant"
+let phonenumber2 = 73373377
+let age2 = 17
+let country2 = "Kuwait"
+let area2 = "mubarak" + " " + "al" + "-" + "kabeer"
+let password2 = "GoodLuckWithThatHackers"
+let isKuwaiti2 = true
+
 
 
 
@@ -60,7 +70,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
+let firstname3 = "omar"
+let lastname3 = "almulla"
+let email3 = "nonexistantx2"
+let phonenumber3 = 73373388
+let age3 = 17
+let country3 = "Kuwait"
+let area3 = "West" + " " + "Mishref"
+let password3 = "GoodLuckWithThatHackersAsWell"
+let isKuwaiti3 = true
 
 
 
@@ -82,3 +100,24 @@ var isKuwaiti = false
 
  
  */
+(age1) == (age2) && (age2) == (age3) && (age1) == (age3)
+(area1) == (area2) && (area2) == (area3) && (area1) == (area3)
+var legalAge = 18
+(age1) > (legalAge) && (age2) > (legalAge) && (age3) > (legalAge)
+let underLegalAge = 17
+(age1) < (legalAge) && (age2) < (legalAge) && (age3) < (legalAge)
+let almostAdult = 17
+(age1) == (almostAdult) && (age2) == (almostAdult) && (age3) == (almostAdult)
+let underAge  =  17
+(age1) < (underAge) && (age2) < (underAge) && (age3) < (underAge)
+(age1) != (almostAdult) && (age2) != (almostAdult) && (age3) != (almostAdult)
+
+
+
+
+
+
+
+
+
+
